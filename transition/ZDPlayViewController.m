90255247bf7b9ef00fea092e847f399167495c1d
赵东亮 <3012287667@qq.com>
2016-09-03 12:54:23 +0800

    3. 手势实现转场动画

--- transition/ZDPlayViewController.m
+++ transition/ZDPlayViewController.m
@@ -31,8 +31,49 @@
     [self.view addSubview:btn];
     
     
+    
+    // 通过拖拽按钮实现转场动画
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(changeViewFrame:)];
+    [self.view addGestureRecognizer:panGesture];
 }
 
+
+///  转场动画方法实现
+- (void)changeViewFrame:(UIPanGestureRecognizer *)pan {
+
+    // 拖拽的距离
+    CGPoint point = [pan translationInView:pan.view];
+    // 旋转的角度
+    CGFloat scale = point.x / [UIScreen mainScreen].bounds.size.width;
+    NSLog(@"%f", scale);
+    
+    self.view.transform = CGAffineTransformMakeRotation(scale * M_PI_4 / 2);
+    
+    // 判断状态
+    // 当手势停止，或者手势取消时
+    if (pan.state == UIGestureRecognizerStateEnded || pan.state == UIGestureRecognizerStateCancelled) {
+        
+        [UIView animateWithDuration:0.125 animations:^{
+           
+            if (ABS(self.view.transform.b) < 0.16) {
+                
+                self.view.transform = CGAffineTransformIdentity;
+            }else {
+            
+                if (self.view.transform.b < 0) {
+                    
+                    self.view.transform = CGAffineTransformMakeRotation(-M_PI_2);
+                }else {
+                
+                    self.view.transform = CGAffineTransformMakeRotation(M_PI_2);
+                }
+            
+            }
+            
+            
+        }];
+    }
+}
 
 ///  按钮点击事件
 - (void)btnClick {
